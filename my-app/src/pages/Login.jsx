63a354c406834d056dbb5b0ac4ee96bbab498605
GDavidDias <guillermo.david.dias@gmail.com>
2023-11-18 23:42:47 -0300

    page login

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Button, Text, TextInput, View, StyleSheet} from 'react-native'
 import { Formik, useField } from 'formik'
 import StyledTextInput from '../components/StyledTextInput'
+import { loginValidationSchema } from '../validationSchemas/login.js'
+import StyledText from '../components/StyledText.jsx'
 
 const initialValues = {
     email:'',
@@ -11,23 +13,33 @@ const initialValues = {
 const styles = StyleSheet.create({
     form:{
         margin:12,
+    },
+    error:{
+        color:'red',
+        fontSize:12,
+        marginBottom:20,
+        marginTop:-10
     }
 })
 
 const FormikInputValue = ({name,...props})=>{
     const[field,meta,helpers] = useField(name)
     return(
-        <StyledTextInput
-            value={field.value}
-            onChangeText={value=>helpers.setValue(value)}
-            {...props}
-        />
+        <>
+            <StyledTextInput
+                value={field.value}
+                error={meta.error}
+                onChangeText={value=>helpers.setValue(value)}
+                {...props}
+            />
+            {meta.error && <StyledText style={styles.error}>{meta.error}</StyledText>}
+        </>
     )
 }
 
 export default function LogInPage(){
     return (
-    <Formik initialValues={initialValues} onSubmit={values=>console.log(values)}>
+    <Formik validationSchema={loginValidationSchema} initialValues={initialValues} onSubmit={values=>console.log(values)}>
         {({handleSubmit})=>{
             return <View style={styles.form}>
                 <FormikInputValue 
